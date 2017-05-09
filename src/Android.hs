35832121764a6a8d4eabb566a93b058c6de5ee62
Ali Abrar <aliabrar@gmail.com>
2017-05-09 15:13:40 -0400

    Build with newer packages

--- src/Android.hs
+++ src/Android.hs
@@ -2,9 +2,9 @@
 
 module Android (pushMess) where 
 
-import Network.HTTP.Conduit (newManager, parseUrl, def, httpLbs, method, requestHeaders, requestBody, responseHeaders, RequestBody(RequestBodyLBS))
-import Data.Conduit (runResourceT)
+import Network.HTTP.Conduit (newManager, parseUrlThrow, httpLbs, method, requestHeaders, requestBody, responseHeaders, RequestBody(RequestBodyLBS), tlsManagerSettings)
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Resource (runResourceT)
 import Network.HTTP.Types.Header (ResponseHeaders)
 
 import qualified Data.ByteString as BS
@@ -18,8 +18,8 @@ pushMess :: BS.ByteString -> LBS.ByteString -> IO ResponseHeaders
 pushMess apikey payload =
    runResourceT $ do
      liftIO $ print payload
-     manager <- liftIO $ newManager def
-     req <- liftIO $ parseUrl "https://android.googleapis.com/gcm/send"
+     manager <- liftIO $ newManager tlsManagerSettings
+     req <- liftIO $ parseUrlThrow "https://android.googleapis.com/gcm/send"
      res <- httpLbs req {method = "POST",
                          requestHeaders = [("Authorization", BS.append "key=" apikey),
                                            ("Content-Type", "application/json")],
